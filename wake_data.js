a6ee6b8470940e8abe4e6a54f19bcd2e89815ed8
Santhosh Varathan <sanvsam11@gmail.com>
2022-01-30 13:44:56 +0530

    removed hardcoded values

--- wake_data.js
+++ wake_data.js
@@ -1,7 +1,9 @@
 var wol = require('node-wol')
+require('305').config()
+
 var wake_data = ()=>{
     console.log("Initiating Wake")
-    wol.wake('D4-3D-7E-BC-A6-15', function(error){
+    wol.wake(process.env.DATA_MAC_ID, function(error){
         if(error){
             console.log(error)
             console.log("sync failed")
